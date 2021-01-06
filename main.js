bc639e138522fb883ce9a3f9c07033b7f1ab4441
kondrashov123 <63396439+kondrashov123@users.noreply.github.com>
2021-01-06 14:48:08 +0300

    edit menu-burger

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ $(document).ready(function (){
 	$('.arrow_to_top').click(function(){
 		$('html,body').animate({scrollTop:0},900);
 	});
+	$('.burger').click(function(){
+		$('.menu_list,.burger,body').toggleClass('active');
+	})
 });
